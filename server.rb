af992c5c72f669df9f73066a81a8970278418818
Fadie Hannona <fadiehannona@gmail.com>
2014-10-07 14:01:04 +0100

    filtering by tag testing added

--- server.rb
+++ server.rb
@@ -31,3 +31,9 @@ end
 	Link.create(:url => url, :title => title, :tags => tags)
 	redirect to('/')
 end
+
+get '/tags/:text' do
+	tag = Tag.first(:text => params[:text])
+	@links = tag ? tag.links : []
+	erb :index
+end
