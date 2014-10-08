393f44659b09fb77efeb5bceabe014271a5948b3
Fadie Hannona <fadiehannona@gmail.com>
2014-10-08 11:55:42 +0100

    Added user accounts

--- lib/tag.rb
+++ lib/tag.rb
@@ -2,9 +2,9 @@ class Tag
 
 	include DataMapper::Resource
 
-	has n, :links, :through => Resource
+	has n, :links, :through => Resource # has many relationships.
 
 	property :id, Serial
 	property :text, String
-	
+
 end
