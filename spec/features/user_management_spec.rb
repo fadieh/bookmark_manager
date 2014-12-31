09133fe787d04440c2ab714932ac6f2b69d247ba
Fadie Hannona <fadiehannona@gmail.com>
2014-12-31 15:44:51 +0000

    fixed spec

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -47,16 +47,6 @@ end
 
 feature "User signs up" do
 
-	# Strictly speaking, the tests that check the UI
-	# (have_content, etc.) should be seperate from the tests
-	# that check what we have in the DB. The reason is that
-	# you should test one thing at a time, whereas
-	# by mixing the two, we're testing both
-	# the business logic and the views.
-	#
-	# However, let's not worry about this yet
-	# to keep the example simple.
-
 	scenario "when being logged out" do
 		expect { sign_up }.to change(User, :count).by(1)
 		expect(page).to have_content("Welcome, alice@example.com")
@@ -81,7 +71,7 @@ feature "User clicks forgotten password" do
 
 	scenario "enter their email into the forgotten pass form" do
 		visit '/'
-		expect(page).to have_content("Forgotten your password?")
+		expect(page).to have_content("Forgot password?")
 		click_button "Forgot password?"
 		expect(page).to have_content("Please enter your email")
 	end
